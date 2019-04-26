5faf607c2c61b5e16bd4a26c3b7c53e9749835aa
patriciamesttre <patriciamesttre@hotmail.com>
2019-04-26 19:37:02 -0300

    Destruindo a bolinha ao tocar o chão

---
+++ Arkanoid/Assets/Scripts/Destruidor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruidor : MonoBehaviour
+{
+   void OnCollisionEnter2D(Collision2D collision)
+   {
+       GameObject collider = collision.collider.gameObject;
+       GameObject.Destroy(collider);
+   }
+}
