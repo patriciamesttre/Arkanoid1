f2c4db800d687d3255606aed2d895e7cbc95731b
patriciamesttre <patriciamesttre@hotmail.com>
2019-04-26 18:00:44 -0300

    arkanoid

---
+++ Arkanoid/Assets/Scripts/Bola.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+    public Vector2 velocidade;
+ void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(velocidade);
+    }
+}
---
+++ Arkanoid/Assets/Scripts/Jogador.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public float velocidade = 10.0f;
+
+    public float horizontalAxis;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(velocidade * horizontalAxis, 0);
+    }
+}
